d73b85a2b7058685df8520d6f9a46e5aa888fdbd
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-10-04 10:17:20 -0400

    set up fileUtils.h

---
+++ include/fileUtils.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 8192 // 8 KB
+
+typedef enum {
+    FILE_SUCCESS = 0,
+    FILE_NOT_FOUND = -1,
+    FILE_READ_ERROR = -2,
+    FILE_WRITE_ERROR = -3
+} FileResult;
+
+char* readInputFile(const char* filepath);
+FileResult writeToFile(const char* filepath, const char* content);
+
