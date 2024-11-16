6b885174fc51c27016f02f87c13587a2a6ad0e81
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-11-16 01:03:27 -0500

    update asm codeGen for new operators

--- src/codeGen.c
+++ src/codeGen.c
@@ -16,21 +16,52 @@ void freeCodeGenContext(CodeGenContext* context) {
 void generateProgram(CodeGenContext* context, AstNode* node) {
     fprintf(context->output, "global _main\n");
     fprintf(context->output, "section .text\n");
-    fprintf(context->output, "_main:\n");    
+    fprintf(context->output, "_main:\n");
+    fprintf(context->output, "    push rbp\n");
+    fprintf(context->output, "    mov rbp, rsp\n");
+    fprintf(context->output, "    sub rsp, 128\n");
     for (size_t i = 0; i < node->as.program.count; i++) {
         generateCode(context, node->as.program.stmts[i]);
     }
+    fprintf(context->output, "    leave\n");
+    fprintf(context->output, "    and rsp, -16\n");
 }
+
 
 void generateReturn(CodeGenContext* context, AstNode* node) {
-    generateExpression(context, node->as.retStmt.val);
-    fprintf(context->output, "    ret\n");
+    generateExpression(context, node->as.retStmt.val,8);
+    // fprintf(context->output, "    ret\n");
+    fprintf(context->output, "    mov rdi, rax\n");
+    fprintf(context->output, "    mov rax, 0x2000001\n");
+    fprintf(context->output, "    syscall\n");
 }
 
-void generateExpression(CodeGenContext* context, AstNode* node) {
+void generateExpression(CodeGenContext* context, AstNode* node, int stackOffset) {
     switch (node->type) {
         case AST_NUM:
             fprintf(context->output, "    mov rax, %d\n", node->as.num.val);
+            break;
+        case AST_BINOP:
+            generateExpression(context, node->as.binop.left, stackOffset);
+            fprintf(context->output, "    mov [rbp - %d], rax\n", stackOffset);
+            generateExpression(context, node->as.binop.right, stackOffset + 8);
+            fprintf(context->output, "    mov rbx, rax\n");
+            fprintf(context->output, "    mov rax, [rbp - %d]\n", stackOffset);
+            switch (node->as.binop.op) {
+                case OP_ADD:
+                    fprintf(context->output, "    add rax, rbx\n");
+                    break;
+                case OP_SUB:
+                    fprintf(context->output, "    sub rax, rbx\n");
+                    break;
+                case OP_MULT:
+                    fprintf(context->output, "    imul rax, rbx\n");
+                    break;
+                case OP_DIV:
+                    fprintf(context->output, "    cqo\n");
+                    fprintf(context->output, "    idiv rbx\n");
+                    break;
+            }
             break;
         default:
             fprintf(stderr, "Error: Unsupported expression type in code generation\n");
