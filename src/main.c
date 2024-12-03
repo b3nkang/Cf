580cca05a78a98ad4e8f3ad0d7b74c898d51f843
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-12-03 12:45:04 -0500

    refactor main to work with make

--- src/main.c
+++ src/main.c
@@ -17,6 +17,13 @@ void compileCf (const char* sourceFile, const char* outputFile) {
             break;
         }
     }
+    // printf("Tokens received:\n");
+    // for (size_t i = 0; i < tokArr->count; i++) {
+    //     printf("Token %zu: Type = %d, Value = %s\n", 
+    //         i, 
+    //         tokArr->tokens[i]->type,
+    //         tokArr->tokens[i]->value ? tokArr->tokens[i]->value : "NULL");
+    // }
     AstNode* programAst = parse(tokArr, sourceFile);
     printf("----------------\n");
     printf("AST Structure:\n");
@@ -38,44 +45,39 @@ void compileCf (const char* sourceFile, const char* outputFile) {
         printf("%zu: Type = ", i);
         switch (tokArr->tokens[i]->type) {
             case NUM_TOK:
-                printf("NUMBER, Value = %s\n", tokArr->tokens[i]->value);
-                break;
+                printf("NUMBER, Value = %s\n", tokArr->tokens[i]->value); break;
             case SEMI_TOK:
-                printf("SEMICOLON\n");
-                break;
+                printf("SEMICOLON\n"); break;
             case EOF_TOK:
-                printf("EOF\n");
-                break;
+                printf("EOF\n"); break;
             case RET_TOK:
-                printf("RETURN, Value = %s\n", tokArr->tokens[i]->value);
-                break;
+                printf("RETURN, Value = %s\n", tokArr->tokens[i]->value); break;
             case DIV_TOK:
-                printf("DIV\n");
-                break;
+                printf("DIV\n"); break;
             case MULT_TOK:
-                printf("MULT\n");
-                break;
+                printf("MULT\n"); break;
             case PLUS_TOK:
-                printf("PLUS\n");
-                break;
+                printf("PLUS\n"); break;
             case MINUS_TOK:
-                printf("MINUS\n");
-                break;
+                printf("MINUS\n"); break;
             case LPAREN_TOK:
-                printf("LPAREN\n");
-                break;
+                printf("LPAREN\n"); break;
             case RPAREN_TOK:
-                printf("RPAREN\n");
-                break;
+                printf("RPAREN\n"); break;
             case EQ_TOK:
-                printf("EQ\n");
-                break;
+                printf("EQ\n"); break;
             case VAR_TOK:
-                printf("VAR\n");
-                break;
+                printf("VAR\n"); break;
             case IDENT_TOK:
-                printf("IDENT\n");
-                break;
+                printf("IDENT\n"); break;
+            case IF_TOK:
+                printf("IF\n"); break;
+            case ELSE_TOK:
+                printf("ELSE\n"); break;
+            case LBRACE_TOK:
+                printf("LBRACE\n"); break;
+            case RBRACE_TOK:
+                printf("RBRACE\n"); break;
             default:
                 printf("UNKNOWN\n");
         }
@@ -89,11 +91,11 @@ void compileCf (const char* sourceFile, const char* outputFile) {
 }
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr,"Wrong format, correct is: cf <inputFile.cf>");
+    if (argc != 3) {
+        fprintf(stderr,"Wrong format, correct is: cf <inputFile.cf> <asmOutputName.asm>");
         return EXIT_FAILURE;
     }
     printf("Ingesting input file: %s\n",argv[1]);
-    compileCf(argv[1],"test4.asm");
+    compileCf(argv[1],argv[2]);
     return EXIT_SUCCESS;
 }
